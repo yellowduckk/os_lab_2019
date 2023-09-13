e6875dcdb3abd0b35a01726dd98f372371a9fabe
tata <51916537+yellowduckk@users.noreply.github.com>
2023-09-13 12:50:26 +0000

    2 lab 2 task

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,32 @@
 #include "revert_string.h"
+#include <stdio.h>
 
 void RevertString(char *str)
 {
 	// your code here
+  int len=0, n;
+  char temp;
+ // len=strlen(str);
+  
+  
+  for (int i = 0; str[i] != '\0'; i++){
+      len = i;
+      n = i;
+  }
+  len += 1;
+// printf("%d %d", len, n);
+  int a =5;
+  
+   for(int i = 0; i <=(len/2); i++)
+  {
+    temp=str[i];
+    str[i]=str[n];
+    str[n]=temp;
+    n--;
+  }
+  
+  for (int i =0; i<len; i++){
+      printf("%c", str[i]);
+  }
 }
 
