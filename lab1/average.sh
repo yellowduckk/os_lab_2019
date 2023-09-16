49e97ce4a2a378240c9033751d8df8acac0e177f
tata <51916537+yellowduckk@users.noreply.github.com>
2023-09-16 13:25:56 +0000

    lab1 4 task

--- lab1/average.sh
+++ lab1/average.sh
@@ -3,19 +3,15 @@
 #echo "Кол-во параметров:" $#
 echo "Введено: $@"
 
-count=0
-sum=0
-
-while read param
+for param in $@
 do
-
-count=$(($count+1))
-sum=$(($sum+$param))
-
+#echo "\$@ Parameter #$count = $param"
+sum=$(( $sum + $param))
+count=$(( $count + 1 ))
 done
 
 echo "Кол-во элементов: " $count
 echo "сумма: " $sum
+let "sred=$sum/$count" 
 
-let "sred=$sum/$count"
 echo "Среднее арифметическое: $sred"
